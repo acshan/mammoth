e9e30df4f6607d5cd2ffb1a95cad4f2abc6fe932
sage.ace <sage.ace@outlook.com>
2018-04-26 13:50:54 +0800

    +adjust

--- gads/src/main/java/com/google/sage/gads/controller/indexController.java
+++ gads/src/main/java/com/google/sage/gads/controller/indexController.java
@@ -3,20 +3,22 @@ package com.google.sage.gads.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class indexController {
 
     @RequestMapping("/clicktobuy")
-    public String greeting(Model model) {
+    public String clickToBuy(Model model) {
 //        model.addAttribute("name", name);
         return "clicktobuy/index";
     }
 
-//    @GetMapping("/")
-//    public String hello() {
-//        return "Hello Spring Boot!";
-//    }
+
+    @RequestMapping("/clicktobuy/callback")
+    public String ctbCallback(Model model) {
+        return "clicktobuy/callback";
+    }
+
+
 }
