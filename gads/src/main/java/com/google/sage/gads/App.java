ce81033858fd4a9f46427dae3c9709320cbac2b8
sage.ace <sage.ace@outlook.com>
2018-04-26 13:39:39 +0800

    +adjust

--- gads/src/main/java/com/google/sage/gads/App.java
+++ gads/src/main/java/com/google/sage/gads/App.java
@@ -2,19 +2,26 @@ package com.google.sage.gads;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class App {
+public class App  extends SpringBootServletInitializer {
 
     public static void main(String[] args){
         SpringApplication.run(App.class,args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
+
     @GetMapping("/")
     public String hello() {
-        return "Hello Spring Boot!";
+        return "Hello Googler!";
     }
 }
